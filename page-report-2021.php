ead0717982e18d87ed769323cc97484cc85c55d6
Tom607 <thomasmarkaustin@gmail.com>
2021-02-19 18:59:28 +0000

    finished report page

--- page-report-2021.php
+++ page-report-2021.php
@@ -16,66 +16,171 @@ if (have_posts()) :
     <section class="overview_description description_sect">
       <div class="container">
         <div class="row">
-          <div class="col-lg-6 offset-lg-1" data-aos="fade-left">
+          <div class="col-lg-10 offset-lg-1" data-aos="fade-left">
             <h1>
               Causes That Count Index 2021
             </h1>
 
-            <h4 data-aos="fade-left" data-aos-delay="100">
-              Causes That Count Index 2021
-            </h4>
+
           </div>
         </div>
         <div class="row">
-          <div class="col-lg-8 col-md-10 offset-lg-1" data-aos="fade-up" data-aos-delay="200">
+          <div class="col-lg-6 col-md-10 offset-lg-2" data-aos="fade-up" data-aos-delay="200">
+            <h3>
+              The 50 issues that matter most to people this year
+            </h3>
             <p class="grey">At Revolt, we’re often asked by our clients how they can stand for something that matters to their audience. Following a landmark year, it felt like an opportune moment to pause and take stock of what people really care about. The Causes That Count 2021 study surveyed 1600 people across the US, the UK, China and Brazil to paint a uniquely global picture. We hope that this annual index will provide further insights in the years to come.</p>
 
           </div>
         </div>
       </div>
     </section>
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-10 offset-lg-1" data-aos="fade-up" data-aos-delay="100">
+            <img src="https://revoltlondon.com/wp-content/uploads/2021/02/RE_TheCausesThatCount_1920x1080_210210_MD_MOCK-scaled.jpg" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="container table-content">
+        <div class="row">
+          <div class="col-lg-6 col-md-10 offset-lg-2">
+            <h4 data-aos="fade-up">The Index</h4>
+            <p data-aos=" fade-up" data-aos-delay="200">We worked with research company Attest to ask 1600 people across 4 markets to tell us which issues they found to be the most important to them. Split into two parts, we asked for unprompted answers to the question, before prompting them with 50 predefined issues. </p>
+            <p data-aos="fade-up" data-aos-delay="200">These prompted issues were ranked on a scale of 5 from ‘one of the most important’ to ‘not important at all’ - each response was assigned a corresponding value to create total scores for each issue, as follows: </p>
+            <table data-aos="fade-up" data-aos-delay="300">
+              <th>Response</th>
+              <th>Value</th>
+              <tr>
+                <td>Unprompted answer</td>
+                <td>7</td>
+              </tr>
+              <tr>
+                <td>One of the most important</td>
+                <td>5</td>
+              </tr>
+              <tr>
+                <td>Important</td>
+                <td>3</td>
+              </tr>
+              <tr>
+                <td>Somewhat important</td>
+                <td>1</td>
+              </tr>
+              <tr>
+                <td>Not that important</td>
+                <td>-3</td>
+              </tr>
+              <tr>
+                <td>Not important at all</td>
+                <td>-5</td>
+              </tr>
+            </table>
+            <p data-aos="fade-up" data-aos-delay="200">We have presented a topline breakdown of the data at global and market level, as well as total scores, in the table below. However, if you’d like to read our analysis and its implications for brands, we have compiled our findings into a 30 page PDF report, available at the bottom of this page. </p>
+            <div class="row pb-5">
+              <div class="col-6">
+                <table data-aos="fade-up" data-aos-delay="300">
+                  <th>Markets</th>
+                  <tr>
+                    <td>USA</td>
+                  </tr>
+                  <tr>
+                    <td>UK</td>
+                  </tr>
+                  <tr>
+                    <td>China</td>
+                  </tr>
+                  <tr>
+                    <td>Brazil</td>
+                  </tr>
+                </table>
+              </div>
+              <div class="col-6">
+                <table data-aos="fade-up" data-aos-delay="400">
+                  <th>Categories</th>
+                  <tr>
+                    <td>Environmental</td>
+                  </tr>
+                  <tr>
+                    <td>Political</td>
+                  </tr>
+                  <tr>
+                    <td>Technology</td>
+                  </tr>
+                  <tr>
+                    <td>Economic</td>
+                  </tr>
+                  <tr>
+                    <td>Social</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+    </section>
 
 
     <section>
       <div class="container">
         <div class="row">
-          <div class="col-md-10 offset-lg-1">
+          <div class="col-md-10 offset-lg-1" data-aos="fade-up" data-aos-delay="100">
             <div id="report-2021-table"></div>
           </div>
     </section>
 
     <section>
+      <div class="container pb-5">
+        <div class="row">
+          <div class="col-lg-6 col-md-10 offset-lg-2">
+            <h3 data-aos="fade-up" data-aos-delay="100">Understanding what matters</h3>
+
+            <p data-aos="fade-up" data-aos-delay="200">To help interpret the data, our report takes a closer look at each issue and how various factors could have led to its global rank, starting from #50. To support the learnings from these findings, we have also peppered in analysis per market, key themes for 2021 and what we can expect in 2022.</p>
+          </div>
+          <div class="col-md-10 offset-lg-1 py-5" data-aos="fade-up" data-aos-delay="100">
+            <img src="https://revoltlondon.com/wp-content/uploads/2021/02/RE_TheCausesThatCount_1920x1080_210210_MD_6-scaled.jpg" alt="">
+          </div>
+          <div class="col-lg-6 col-md-10 offset-lg-2">
+            <h6 data-aos="fade-up" data-aos-delay="100">Insights have included:</h6>
+            <ul>
+              <li data-aos="fade-up" data-aos-delay="100">44% of people globally felt climate change was one of the most important issues even in the midst of a global pandemic.</li>
+              <li data-aos="fade-up" data-aos-delay="200">China was the only country where natural disaster relief and prevention just made it into the top 10 issues for our respondents - indicative of a difficult year for their physical environment.</li>
+              <li data-aos="fade-up" data-aos-delay="300">Pre-Biden administration, 50% of US respondents thought ‘government transparency and threats to democracy’ was one of the most important issues, compared to just 29% of UK respondents.</li>
+              <li data-aos="fade-up" data-aos-delay="400">For the two markets where gun ownership is legal, crime, violence and gun control ranked #4 in Brazil, but only sat at #15 most important in the US.</li>
+            </ul>
+          </div>
+          <div class="col-md-10 offset-lg-1 pt-5" data-aos="fade-up" data-aos-delay="100">
+            <img src="https://revoltlondon.com/wp-content/uploads/2021/02/RE_TheCausesThatCount_1920x1080_210210_MD_15-scaled.jpg" alt="">
+          </div>
+          <div class="col-md-10 offset-lg-1 pt-5" data-aos="fade-up" data-aos-delay="100">
+            <img src="https://revoltlondon.com/wp-content/uploads/2021/02/RE_TheCausesThatCount_1920x1080_210210_MD_23-scaled.jpg" alt="">
+          </div>
+        </div>
+    </section>
+
+
+    <section>
       <div class="container">
         <div class="row">
-          <div class="col-md-6 offset-lg-1">
-            <!-- Begin Mailchimp Signup Form -->
-            <div id="mc_embed_signup">
-              <form action="https://revoltlondon.us5.list-manage.com/subscribe/post?u=04ea4594ec3a0711214c8b5d8&amp;id=31395af80d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                <div id="mc_embed_signup_scroll">
+          <div class="col-lg-4 col-md-5 offset-lg-1" data-aos="fade-up" data-aos-delay="100">
+            <h4>Download the Report</h4>
 
-                  <div class="mc-field-group">
-                    <label for="mce-EMAIL">Email Address </label>
-                    <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-                  </div>
-                  <div class="mc-field-group">
-                    <label for="mce-FNAME">First Name </label>
-                    <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
-                  </div>
-                  <div class="mc-field-group">
-                    <label for="mce-LNAME">Last Name </label>
-                    <input type="text" value="" name="LNAME" class="" id="mce-LNAME">
-                  </div>
-                  <div id="mce-responses" class="clear">
-                    <div class="response" id="mce-error-response" style="display:none"></div>
-                    <div class="response" id="mce-success-response" style="display:none"></div>
-                  </div> <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                  <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_04ea4594ec3a0711214c8b5d8_31395af80d" tabindex="-1" value=""></div>
-                  <div class="clear"><input type="submit" value="Download" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-                </div>
-              </form>
-            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 col-md-5 offset-lg-1 pb-5" data-aos="fade-up" data-aos-delay="200">
+            <p>We are really excited to share this report with you, in return we ask you to leave a contact email address which we will use to keep you up to date with future reports and findings directly from Revolt via our monthly newsletter. We do not share this information and you can unsubscribe at any time. For more information please read our <a href="<?php echo esc_url(get_page_link(3)); ?>">Privacy Policy.</a></p>
+            <p class="grey" data-aos="fade-up" data-aos-delay="300">Should you be interested in further breakdowns of the Index (eg. issue rankings by gender, household income etc), or want further insights that could benefit your business, please get in touch by emailing <a href="mailto:alex@revoltlondon.com">alex@revoltlondon.com</a></p>
 
-            <!--End mc_embed_signup-->
+          </div>
+          <div class="col-lg-5 col-md-6 offset-lg-1" data-aos="fade-up" data-aos-delay="300">
+            <!-- Mailchimp Form -->
+            <?php echo do_shortcode("[mc4wp_form id='1688']"); ?>
           </div>
         </div>
       </div>
